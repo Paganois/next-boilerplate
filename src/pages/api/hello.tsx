7f7665cccd5874ccf93bafb6a2f5ce696261b688
Gabriel Henrique <contato@gabrielhenrique.net>
2021-02-15 19:20:23 -0300

    Add console log in req api

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    console.log(req);
     res.status(200).json({ name: "John Doe" });
 };
 
