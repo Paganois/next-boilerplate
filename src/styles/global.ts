4b622f19e35029f112678214a93f161e88707e6c
Gabriel Henrique <contato@gabrielhenrique.net>
2021-02-21 09:09:16 -0300

    Add packages, config styled components and fix configs

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,22 @@ export default createGlobalStyle`
         Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     }
 
+    html {
+        font-size: 65%;
+    }
+
+    html, body, #__next {
+        height: 100%;
+    }
+
     a {
         color: inherit;
         text-decoration: none;
     }
 
     * {
+        margin: 0;
+        padding: 0;
         box-sizing: border-box;
     }
 `;
