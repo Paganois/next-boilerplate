9654bc9b7957c78da7597a988b4d6a6df5fa0008
Gabriel Henrique <contato@gabrielhenrique.net>
2021-02-21 11:09:39 -0300

    change favicon and add extra icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,13 +12,36 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <ThemeProvider theme={theme}>
             <Head>
                 <title>Onyx - Next Boilerplate</title>
+                {/* Use this site to generate favicons -> https://realfavicongenerator.net/ */}
                 <link
                     rel="shortcut icon"
                     href="/img/icon-512.png"
                     type="image/x-icon"
                 />
-                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/img/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/img/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/img/favicon-16x16.png"
+                />
+                <link
+                    rel="mask-icon"
+                    href="/img/safari-pinned-tab.svg"
+                    color="#1e0b36"
+                />
                 <link rel="manifest" href="manifest.json" />
+                <meta name="msapplication-TileColor" content="#1e0b36" />
                 <meta
                     name="description"
                     content="Marketplace for multiple purposes"
