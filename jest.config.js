c429540487b5a7c15e2375a4ab2b0a5a877d7d5e
Buz <contato@gabrielhenrique.net>
2021-02-24 16:06:03 -0300

    fix test and run scripts

--- jest.config.js
+++ jest.config.js
@@ -14,8 +14,4 @@ module.exports = {
         "jest-watch-typeahead/filename",
         "jest-watch-typeahead/testname",
     ],
-    moduleNameMapper: {
-        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-        "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
-    },
 };
