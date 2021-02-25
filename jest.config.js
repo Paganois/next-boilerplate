7e23e943a8c23202ef17d8173f8c96bd3d22a2c8
Buz <contato@gabrielhenrique.net>
2021-02-25 00:01:10 -0300

    add extra rules and remove files to coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 module.exports = {
     testEnvironment: "jsdom",
     collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.ts(x)?", "!src/**/*.stories.tsx"],
+    collectCoverageFrom: [
+        "src/**/*.ts(x)?",
+        "!src/**/*.stories.tsx",
+        "!src/pages/**/*.tsx",
+        "!src/styles/**/*.ts",
+        "!src/types/**/*.d.ts",
+    ],
     setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
-    roots: ["<rootDir>"],
+    modulePaths: ["<rootDir>/src/", "<rootDir>/.jest"],
     moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+    moduleNameMapper: {
+        "^styled-components":
+            "<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js",
+    },
     testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
     transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
     transform: {
