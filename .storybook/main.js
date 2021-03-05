d0ab2719dedb5435a469987ca54a2d74ca335f3d
Buz <contato@gabrielhenrique.net>
2021-03-05 15:32:46 -0300

    optimization work with storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  webpackFinal: (config) => {
-      config.resolve.modules.push(`${process.cwd()}/src`)
-      return config
-  }
-}
+    stories: [
+        "../src/components/**/*.stories.mdx",
+        "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    babel: async (options) => ({
+        ...options,
+        plugins: [...options.plugins, "@babel/plugin-transform-react-jsx"],
+    }),
+    webpackFinal: (config) => {
+        config.resolve.modules.push(`${process.cwd()}/src`);
+        return config;
+    },
+};
